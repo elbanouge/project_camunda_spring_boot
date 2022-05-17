cb5bd29130a512c0f9dfdf61a0f25aeb39e526e2
Abdellah Elbanouge <abde.banouge@gmail.com>
2022-05-17 18:24:13 +0100

    test

--- src/main/java/com/camunda/bpm/workflow/delagate/EnvoiOTPDelagate.java
+++ src/main/java/com/camunda/bpm/workflow/delagate/EnvoiOTPDelagate.java
@@ -1,12 +1,21 @@
 package com.camunda.bpm.workflow.delagate;
 
+import lombok.extern.log4j.Log4j2;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
+@Log4j2
 public class EnvoiOTPDelagate implements JavaDelegate {
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
-
+        log.info("\n\n ... LoggerDelegate invoked by "
+                + " activtyName='" + delegateExecution.getCurrentActivityName()
+                + " activtyId=" + delegateExecution.getCurrentActivityId()
+                + "  processDefinitionId=" + delegateExecution.getProcessDefinitionId()
+                + "   processInstanceId=" + delegateExecution.getProcessInstanceId()
+                + "  businesskey=" + delegateExecution.getProcessBusinessKey()
+                + "    executionId=" + delegateExecution.getId()
+                + "    variables=" + delegateExecution.getVariables());
     }
 }
---
+++ src/main/java/com/camunda/bpm/workflow/listener/VerifierManuelDocs.java
@@ -0,0 +1,11 @@
+package com.camunda.bpm.workflow.listener;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.ExecutionListener;
+
+public class VerifierManuelDocs implements ExecutionListener {
+    @Override
+    public void notify(DelegateExecution delegateExecution) throws Exception {
+
+    }
+}
